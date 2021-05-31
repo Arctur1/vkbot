601d97faf29be46e1f7d33e50e83148e1f0fdfbb
“AlexeyRozhnov” <rozhnov.alexey@gmail.com>
2021-05-31 19:01:58 +0300

    deleted api keys

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
-token = '708c8376cf69edec9583af8e407122703a38fef0ca4ade31087a3143ae4d47713e5153b677adc0c1f6ce3' # Токен группы
-tokenapp = '51b95a33a74222b468842f1411c0bb114b0cb71a1873e1a9ba7370fd63acee018c802314880011e1559ac' # Токен приложения
-DSN = 'postgresql://rozhn:rozhn@localhost:5432/vkbot' # Данные бд
+token = '' # Токен группы
+tokenapp = '' # Токен приложения
+DSN = '' # Данные бд
